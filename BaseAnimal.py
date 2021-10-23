094206fd8844a23be618b861745761a6b0b1a292
Sam Purkis <sam.purkis@hotmail.co.uk>
2021-10-23 01:08:54 +0100

    add move action

--- BaseAnimal.py
+++ BaseAnimal.py
@@ -1,11 +1,21 @@
 from datetime import datetime as dt
+import random
+from typing import Tuple
 
 
 class BaseAnimal:
-    def __init__(self, colour: str = "black"):
+    def __init__(self, colour: str = "black", position: Tuple[int, int] = None):
         self.speed = 1
         self.colour = colour
         self.creation_time = dt.now().time()
+        self.position = position
 
     def __str__(self):
         raise NotImplementedError("This is a Base Class, don't call this!")
+
+    def move(self, direction: str = None):
+        if direction is None:
+            x_step = random.choice(range(-1, 1))
+            y_step = random.choice(range(-1, 1))
+            self.position[0] += x_step
+            self.position[1] += y_step
